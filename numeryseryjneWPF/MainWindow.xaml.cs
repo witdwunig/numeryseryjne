c385eb6a891e6648ec8506eebb29d2d34f9c53f5
MatueszC <mateusz.cimochowski@infotech.edu.pl>
2025-04-13 09:12:58 +0100

    commit

--- numeryseryjneWPF/MainWindow.xaml.cs
+++ numeryseryjneWPF/MainWindow.xaml.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using numeryseryjneWPF.Services;
+using numeryseryjneWPF.Models;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Diagnostics;
 
 namespace SerialTrack
 {
     public partial class MainWindow : Window
     {
+        private ApiService _apiService;
         public ObservableCollection<SerialItem> SerialItems { get; set; }
 
         public MainWindow()
@@ -14,16 +18,54 @@ namespace SerialTrack
             InitializeComponent();
             SerialItems = new ObservableCollection<SerialItem>();
             SerialDataGrid.ItemsSource = SerialItems;
+            InitializeServerDiscovery();
+        }
+
+        private async void InitializeServerDiscovery()
+        {
+            Debug.WriteLine("test");
+            var discoveryService = new ServerDiscoveryService();
+            Debug.WriteLine("Before calling discovery");
+            string? serverUrl = await discoveryService.DiscoverServerAsync();
+            Debug.WriteLine("After");
+
+            if (serverUrl != null) 
+            {
+                _apiService = new ApiService(serverUrl);
+                FetchSerialNumbers();
+            } else
+            {
+                Debug.WriteLine("Server was null");
+            }
+        }
+        private async void FetchSerialNumbers()
+        {
+            try
+            {
+                var serialNumbers = await _apiService.GetSerialsAsync();
+                SerialItems.Clear();
+                foreach (var serial in serialNumbers)
+                {
+                    Debug.WriteLine(serial.CreatedAt);
+                    SerialItems.Add(new SerialItem()
+                    {
+                        SerialNumber = serial.number,
+                        ProductName = serial.name,
+                        DateGenerated = serial.CreatedAt
+
+                    });;
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Error podczas zbierania numerow seryjnych: {ex.Message}");
+            }
         }
 
         public void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-            SerialItems.Add(new SerialItem
-            {
-                SerialNumber = "ABC123456", // Możesz potem wygenerować losowy
-                ProductName = ProductNameTextBox.Text,
-                DateGenerated = DateTime.Now
-            });
+            var serialNumbers = _apiService.GenerateSerialNumberAsync(ProductNameTextBox.Text);
+            Debug.WriteLine(serialNumbers);
+            FetchSerialNumbers();
         }
 
         private void ToggleFiltersVisibility(object sender, RoutedEventArgs e)
@@ -39,12 +81,17 @@ namespace SerialTrack
                 (sender as Button).Content = "Pokaż filtry";
             }
         }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeServerDiscovery();
+        }
     }
 
     public class SerialItem
     {
         public string SerialNumber { get; set; }
         public string ProductName { get; set; }
-        public DateTime DateGenerated { get; set; }
+        public DateTimeOffset DateGenerated { get; set; }
     }
 }
